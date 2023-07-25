58fbba50b7dc56f50eba1ea111b27e88b501215e
Kauan Santos <kauansantosdepontes@gmail.com>
2023-07-25 17:55:23 -0300

    created AuthLayout

--- fe/src/view/layouts/AuthLayout.tsx
+++ fe/src/view/layouts/AuthLayout.tsx
@@ -1,11 +1,35 @@
 import { Outlet } from "react-router-dom";
 
+import illustration from "../../assets/illustration.png";
+import Logo from "../components/Logo";
+
 export default function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
+    <div className='flex w-full items-center h-full'>
+      <div className=' lg:w-1/2 w-full h-full flex flex-col items-center justify-center'>
+        <Logo className='text-gray-500 h-6' />
 
-      <Outlet />
+        <div className='mt-16 w-full max-w-[504px] px-8'>
+          <Outlet />
+        </div>
+      </div>
+
+      <div className=' w-1/2 h-full lg:flex items-center justify-center p-8 relative hidden'>
+        <img
+          src={illustration}
+          alt='illustration'
+          className=' object-cover h-full w-full max-w-[656px] max-h-[960px] select-none rounded-[32px]'
+        />
+
+        <div className='max-w-[656px] bottom-8 w-full bg-white p-10 absolute rounded-b-[32px] text-red-500'>
+          <Logo className='text-teal-900 h-8' />
+
+          <p className='text-gray-700 font-medium text-xl mt-6'>
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck,
+            e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          0: '#F8F9FA',
+          100: '#F1F3F5',
+          200: '#E9ECEF',
+          300: '#DEE2E6',
+          400: '#CED4DA',
+          500: '#ADB5BD',
+          600: '#868E96',
+          700: '#495057',
+          800: '#343A40',
+          900: '#212529',
+        },
+        teal: {
+          0: '#E6FCF5',
+          100: '#C3FAE8',
+          200: '#96F2D7',
+          300: '#63E6BE',
+          900: '#087F5B',
+        }
+      },
+    },
   },
   plugins: [],
 }
