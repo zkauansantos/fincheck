de45e5e10dbd95cf0a4f0d737a75ccf15c608a17
Kauan Santos <kauansantosdepontes@gmail.com>
2023-07-31 18:30:45 -0300

    imported all icons from figma

---
+++ fe/src/view/components/icons/BankAccountTypeIcon/index.tsx
@@ -0,0 +1,13 @@
+import { iconsMap } from "./iconsMap";
+
+interface BankAccountTypeIconProps {
+  type: keyof typeof iconsMap;
+}
+
+export default function BankAccountTypeIcon({
+  type,
+}: BankAccountTypeIconProps) {
+  const Icon = iconsMap[type];
+
+  return <Icon />;
+}
