025eae4d9fe2fb283167b3663a5dcfe310655e7c
Kauan Santos <kauansantosdepontes@gmail.com>
2023-07-31 18:31:04 -0300

    created UserMenu component

---
+++ fe/src/view/components/UserMenu.tsx
@@ -0,0 +1,9 @@
+export default function UserMenu() {
+  return (
+    <div className='bg-teal-50 border border-teal-200 h-12 w-12 rounded-full flex items-center justify-center'>
+      <span className='text-teal-900 font-medium tracking-[-0.5px] text-sm'>
+        KS
+      </span>
+    </div>
+  );
+}
